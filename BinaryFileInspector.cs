69b772b2f9fba8a0ebbfdb55da35dfdae39974df
youn1on <a.shymanska.ip13@kpi.ua>
2022-09-30 18:38:45 +0300

    BinaryFile Inspector added

---
+++ BinaryFileInspector.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+
+namespace Lab1;
+
+public class BinaryFileInspector
+{
+    public static void Inspect(string filename, string outputFile, int inspectedElemNumber)
+    {
+        BinaryReader binaryReader = new BinaryReader(new FileStream(filename, FileMode.Open));
+        StreamWriter streamWriter = new StreamWriter(outputFile);
+
+        FileInfo fileInfo = new FileInfo(filename);
+        long fileSize = fileInfo.Length;
+        if (fileSize > 0)
+        {
+            if (fileSize <= inspectedElemNumber * 4 * 2)
+            {
+                byte[] binData = binaryReader.ReadBytes((int)fileSize);
+                streamWriter.Write(BitConverter.ToInt32(binData[0..4]));
+                for (int i = 1; i < binData.Length / 4; i++)
+                    streamWriter.Write(", " + BitConverter.ToInt32(binData[(i * 4)..((i + 1) * 4)]));
+            }
+            else
+            {
+                byte[] binData = binaryReader.ReadBytes(inspectedElemNumber * 4);
+                streamWriter.Write(BitConverter.ToInt32(binData[0..4]));
+                for (int i = 1; i < inspectedElemNumber; i++)
+                    streamWriter.Write(", " + BitConverter.ToInt32(binData[(i * 4)..((i + 1) * 4)]));
+
+                streamWriter.Write("\n ... \n");
+                binaryReader.BaseStream.Seek(fileSize - inspectedElemNumber * 4, SeekOrigin.Begin);
+
+                binData = binaryReader.ReadBytes(inspectedElemNumber * 4);
+                streamWriter.Write(BitConverter.ToInt32(binData[0..4]));
+                for (int i = 1; i < inspectedElemNumber; i++)
+                    streamWriter.Write(", " + BitConverter.ToInt32(binData[(i * 4)..((i + 1) * 4)]));
+            }
+        }
+
+        binaryReader.Close();
+        streamWriter.Close();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,25 +5,25 @@ namespace Lab1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Stopwatch sw = new Stopwatch();
             sw.Start();
-            FileGenerator.GenerateFile(1);
+            FileGenerator.GenerateFile(200);
             Console.WriteLine($"[File generated: {sw.ElapsedMilliseconds} ms]");
             sw.Restart();
-            BinaryToCsvConverter.ToCsv("generated", "input.csv");
-            Console.WriteLine($"[Input file converted: {sw.ElapsedMilliseconds} ms]");
+            BinaryFileInspector.Inspect("generated", "input.csv", 100);
+            Console.WriteLine($"[Input file inspected: {sw.ElapsedMilliseconds} ms]");
             sw.Restart();
             string file = InputOperations.GetFilepath();
             Console.WriteLine("[Sorting started]");
             sw.Restart();
-            MergeSorter sorter = new ClassicMergeSorter();
+            MergeSorter sorter = new OptimizedMergeSorter();
             sorter.Sort(file, InputOperations.InputtedFileSize);
             Console.WriteLine($"[File sorted: {sw.ElapsedMilliseconds} ms]");
             sw.Restart();
-            BinaryToCsvConverter.ToCsv("generated", "output.csv");
-            Console.WriteLine($"[Output file converted: {sw.ElapsedMilliseconds} ms]");
+            BinaryFileInspector.Inspect("generated", "output.csv", 100);
+            Console.WriteLine($"[Output file inspected: {sw.ElapsedMilliseconds} ms]");
         }
     }
 }
